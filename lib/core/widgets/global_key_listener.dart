3e522793ce9e103346125e0ef6ddd79364bd2e9d
saeed.dahi97 <saeed.dahi97@gmail.com>
2024-11-07 18:33:10 +0400

    Add global key listener

---
+++ lib/core/widgets/global_key_listener.dart
@@ -0,0 +1,48 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ngu_app/core/widgets/dialogs/confirm_dialog.dart';
+
+class GlobalKeyListener extends StatefulWidget {
+  final Widget child;
+  VoidCallback? f1Action;
+
+  GlobalKeyListener({super.key, required this.child, this.f1Action});
+
+  @override
+  State<GlobalKeyListener> createState() => _GlobalKeyListenerState();
+}
+
+class _GlobalKeyListenerState extends State<GlobalKeyListener> {
+  final FocusNode _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    _focusNode.requestFocus();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return KeyboardListener(
+        focusNode: _focusNode,
+        onKeyEvent: (event) {
+          if (event is KeyDownEvent) {
+            // print(event);
+            // HardwareKeyboard.instance
+            //         .isLogicalKeyPressed(LogicalKeyboardKey.altLeft)
+            if (event.logicalKey == LogicalKeyboardKey.f1) {
+              
+            }
+          }
+        },
+        child: widget.child);
+  }
+}
