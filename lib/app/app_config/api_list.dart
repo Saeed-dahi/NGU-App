061136e0b21126c4a159644596dcdf5e2850057a
saeed.dahi97 <saeed.dahi97@gmail.com>
2025-01-27 12:51:00 +0400

    Update api list file

--- lib/app/app_config/api_list.dart
+++ lib/app/app_config/api_list.dart
@@ -1,5 +1,5 @@
 class APIList {
-  static const String baseUrl = "192.168.0.190:8000";
+  static const String baseUrl = "192.168.0.115:8000";
   static const String api = "api/v1/";
   static const String storageUrl = 'http://$baseUrl/storage/';
 
@@ -21,4 +21,5 @@ class APIList {
   static const String unit = "unit";
   static const String product = "product";
   static const String productUnit = "product-unit";
+  static const String invoice = "invoice";
 }
