df5c26e81714e8616aed7f2cb29db8fe6e3aa920
saeed.dahi97 <saeed.dahi97@gmail.com>
2025-01-28 12:35:46 +0400

    Implement invoice data source and repository for crud operation

---
+++ lib/features/inventory/invoices/data/data_sources/invoice_data_source.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:ngu_app/app/app_config/api_list.dart';
+import 'package:ngu_app/core/error/error_handler.dart';
+import 'package:ngu_app/core/network/network_connection.dart';
+import 'package:ngu_app/features/inventory/invoices/data/models/invoice_model.dart';
+
+abstract class InvoiceDataSource {
+  Future<List<InvoiceModel>> getAllInvoices();
+  Future<InvoiceModel> showInvoice(int invoiceId, String? direction);
+  Future<InvoiceModel> createInvoice(InvoiceModel invoiceModel);
+  Future<InvoiceModel> updateInvoice(InvoiceModel invoiceModel);
+}
+
+class InvoiceDataSourceImpl implements InvoiceDataSource {
+  final NetworkConnection networkConnection;
+
+  InvoiceDataSourceImpl({required this.networkConnection});
+
+  @override
+  Future<List<InvoiceModel>> getAllInvoices() async {
+    final response = await networkConnection.get(APIList.invoice, {});
+    var decodedJson = jsonDecode(response.body);
+
+    ErrorHandler.handleResponse(response.statusCode, decodedJson);
+
+    List<InvoiceModel> invoices = decodedJson['data']
+        .map<InvoiceModel>((invoice) => InvoiceModel.fromJson(invoice))
+        .toList();
+
+    return invoices;
+  }
+
+  @override
+  Future<InvoiceModel> showInvoice(int invoiceId, String? direction) async {
+    final response =
+        await networkConnection.get('${APIList.invoice}/$invoiceId', {});
+    var decodedJson = jsonDecode(response.body);
+
+    ErrorHandler.handleResponse(response.statusCode, decodedJson);
+
+    InvoiceModel invoice = InvoiceModel.fromJson(decodedJson['data']);
+
+    return invoice;
+  }
+
+  @override
+  Future<InvoiceModel> createInvoice(InvoiceModel invoiceModel) async {
+    final response = await networkConnection.post(APIList.invoice, {});
+    var decodedJson = jsonDecode(response.body);
+
+    ErrorHandler.handleResponse(response.statusCode, decodedJson);
+
+    InvoiceModel invoice = InvoiceModel.fromJson(decodedJson['data']);
+
+    return invoice;
+  }
+
+  @override
+  Future<InvoiceModel> updateInvoice(InvoiceModel invoiceModel) async {
+    final response = await networkConnection
+        .put('${APIList.invoice}/${invoiceModel.id}', {});
+    var decodedJson = jsonDecode(response.body);
+
+    ErrorHandler.handleResponse(response.statusCode, decodedJson);
+
+    InvoiceModel invoice = InvoiceModel.fromJson(decodedJson['data']);
+
+    return invoice;
+  }
+}
--- lib/features/inventory/invoices/data/repositories/invoice_repository_impl.dart
+++ lib/features/inventory/invoices/data/repositories/invoice_repository_impl.dart
@@ -1,29 +1,41 @@
 import 'package:dartz/dartz.dart';
 import 'package:ngu_app/core/error/failures.dart';
+import 'package:ngu_app/core/helper/api_helper.dart';
+import 'package:ngu_app/features/inventory/invoices/data/data_sources/invoice_data_source.dart';
 import 'package:ngu_app/features/inventory/invoices/domain/entities/invoice_entity.dart';
 import 'package:ngu_app/features/inventory/invoices/domain/repositories/invoice_repository.dart';
 
 class InvoiceRepositoryImpl implements InvoiceRepository {
-  @override
-  Future<Either<Failure, InvoiceEntity>> createInvoice(
-      InvoiceEntity invoiceEntity) {
-    throw UnimplementedError();
-  }
+  final ApiHelper apiHelper;
+  final InvoiceDataSource invoiceDataSource;
+
+  InvoiceRepositoryImpl(
+      {required this.apiHelper, required this.invoiceDataSource});
 
   @override
-  Future<Either<Failure, List<InvoiceEntity>>> getAllInvoices() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<InvoiceEntity>>> getAllInvoices() async {
+    return await apiHelper
+        .safeApiCall(() => invoiceDataSource.getAllInvoices());
   }
 
   @override
   Future<Either<Failure, InvoiceEntity>> showInvoice(
-      int invoiceId, String? direction) {
-    throw UnimplementedError();
+      int invoiceId, String? direction) async {
+    return await apiHelper
+        .safeApiCall(() => invoiceDataSource.showInvoice(invoiceId, direction));
+  }
+
+  @override
+  Future<Either<Failure, InvoiceEntity>> createInvoice(
+      InvoiceEntity invoiceEntity) async {
+    return await apiHelper.safeApiCall(
+        () => invoiceDataSource.createInvoice(invoiceEntity.toModel()));
   }
 
   @override
   Future<Either<Failure, InvoiceEntity>> updateInvoice(
-      InvoiceEntity invoiceEntity) {
-    throw UnimplementedError();
+      InvoiceEntity invoiceEntity) async {
+    return await apiHelper.safeApiCall(
+        () => invoiceDataSource.updateInvoice(invoiceEntity.toModel()));
   }
 }
